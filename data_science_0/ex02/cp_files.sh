394f5f897e0ce679c4f1baef454986dee5da9810
Noe Desprez <ndesprez@c4r1p5.42nice.fr>
2025-01-27 11:15:05 +0100

    a

---
+++ data_science_0/ex02/cp_files.sh
@@ -0,0 +1,4 @@
+docker cp ~/goinfre/subject/customer/data_2022_oct.csv piscineds_postgres:/tmp/
+docker cp ~/goinfre/subject/customer/data_2022_nov.csv piscineds_postgres:/tmp/
+docker cp ~/goinfre/subject/customer/data_2022_dec.csv piscineds_postgres:/tmp/
+docker cp ~/goinfre/subject/customer/data_2023_jan.csv piscineds_postgres:/tmp/
